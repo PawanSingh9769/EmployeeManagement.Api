25e71d2935c4fd4acd556605dfd3d645699a7ef0
PAWAN_SINGH\pawan <pawansingh9769@gmail.com>
2024-03-27 11:46:44 -0500

    Registering RepositoryContext at runtime

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -13,7 +13,12 @@ builder.Services.ConfigureCores();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+
+
 builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
